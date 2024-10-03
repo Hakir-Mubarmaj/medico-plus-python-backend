0832e6e740ca69d7b765081c0fa703624bb6ec1a
Hakir-Mubarmaj <98396213+Hakir-Mubarmaj@users.noreply.github.com>
2024-10-03 20:04:15 +0600

    CORS update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Configuring SQLite database
 basedir = os.path.abspath(os.path.dirname(__file__))
